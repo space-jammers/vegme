85da31335055b2cefbf25dc503a4ba2272f6df65
lnzd17 <lindsaykayedutton@gmail.com>
2017-06-26 15:34:53 -0400

    removed unnecessary classes and helpers

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,7 @@
 class StaticPagesController < ApplicationController
   require 'recipe'
-  require 'recipe_data'
   def index
     @new_recipes = Recipe.new('apples', 3, 'vegetarian')
-    @recipes = RecipeData.new(@new_recipes.search)
-    @recipes_with_calories = @recipes.each_recipe('calories')
+    @recipes = @new_recipes.search
   end
 end
