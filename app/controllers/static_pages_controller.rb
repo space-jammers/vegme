7770187b11c6d8daa19e1811ec4bb74fe92cbab3
lnzd17 <lindsaykayedutton@gmail.com>
2017-06-26 12:42:01 -0400

    added helper method

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,9 +3,5 @@ class StaticPagesController < ApplicationController
   def index
     @new_recipes = Recipe.new('apples', 3, 'vegetarian')
     @recipes = @new_recipes.search
-    @recipe_names = []
-    @recipes['hits'].each do |hit|
-      @recipe_names.push(hit['recipe']['label'])
-    end
   end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,10 @@
 module StaticPagesHelper
+  def get_recipe_names(recipes)
+    recipe_names = []
+    recipes['hits'].each do |hit|
+      recipe_names.push(hit['recipe']['label'])
+    end
+
+    recipe_names
+  end
 end
