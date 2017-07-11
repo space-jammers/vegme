1cede8a3a86a1d252f45ae1064a2ef6c9369064b
lnzd17 <lindsaykayedutton@gmail.com>
2017-07-11 12:29:41 -0400

    added tests for QueryResult

--- spec/lib/tasks/query_result_spec.rb
+++ spec/lib/tasks/query_result_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 require 'tasks/query_result'
-require 'json'
+# require 'json'
 file = File.read('spec/data.json')
 data_hash = JSON.parse(file)
 
@@ -22,17 +22,35 @@ RSpec.describe 'QueryResult' do
   end
 
   describe 'result_created?' do
-    it 'returns true if result is created'
-    it 'returns false if result is not created'
+    it 'returns true if result is created' do
+      expect(QueryResult.result_created?).to eq(true)
+    end
+
+    it 'returns false if result is not created' do
+      QueryResult.store_query_result(nil, nil)
+      expect(QueryResult.result_created?).to eq(false)
+    end
   end
 
   describe 'query_error?' do
-    it 'returns true if there is an error'
-    it 'returns false if there is not an error'
+    it 'returns true if there is an error' do
+      QueryResult.store_query_result(403, nil)
+      expect(QueryResult.query_error?).to eq(true)
+    end
+
+    it 'returns false if there is not an error' do
+      expect(QueryResult.query_error?).to eq(false)
+    end
   end
 
   describe 'no_recipe_found?' do
-    it 'returns true if no recipe is found'
-    it 'returns falise if a recipe is found'
+    it 'returns true if no recipe is found' do
+      QueryResult.store_query_result({ 'count' => 0 }, 'asdf')
+      expect(QueryResult.no_recipe_found?).to eq(true)
+    end
+
+    it 'returns falise if a recipe is found' do
+      expect(QueryResult.no_recipe_found?).to eq(false)
+    end
   end
 end
