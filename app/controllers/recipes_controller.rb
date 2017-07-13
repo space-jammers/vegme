ba1d98dd29bbba6f034273aa38d308491a8fe98d
haleymnatz <haleymnatz@gmail.com>
2017-07-13 17:38:04 -0400

    RecipeDTO change SEVEN for codeship

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,7 @@ class RecipesController < ApplicationController
     if my_recipe
       recipe_call = GetRecipe.new(my_recipe.edamam_uri)
       @recipe = recipe_call.find_recipe[0]
+      puts @recipe.inspect
       @recipe_dto = RecipeDto.new(@recipe)
     else
       render status: :not_found
