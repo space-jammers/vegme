dc47d497adaf418ae5d0f04df61c4b3e5212f107
haleymnatz <haleymnatz@gmail.com>
2017-07-13 17:17:51 -0400

    Recipe DTO change TWO for codeship

--- lib/tasks/recipe_dto.rb
+++ lib/tasks/recipe_dto.rb
@@ -2,12 +2,12 @@ class RecipeDto
   attr_accessor :calories, :image, :ingredient_list, :label, :uri, :url, :yield
 
   def initialize(json_object)
-    # self.calories = json_object['calories']
-    self.image = json_object[3] # ['image']
+    self.calories = json_object['calories'].to_i
+    self.image = json_object['image']
     self.ingredient_list = json_object['ingredientLines']
     self.label = json_object['label']
     self.uri = json_object['uri']
     self.url = json_object['url']
-    self.yield = json_object['yield']
+    self.yield = json_object['yield'].to_i
   end
 end
