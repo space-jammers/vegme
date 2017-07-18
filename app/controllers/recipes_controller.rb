c3025c45b336aa392f0726a0e9594c9c62a41df7
haleymnatz <haleymnatz@gmail.com>
2017-07-18 15:56:54 -0400

    Added comment for future changes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,6 +5,7 @@ class RecipesController < ApplicationController
     if recipe
       @recipe = RecipesHelper.saved_recipe_from_api(recipe)
     else
+      # can redirect back to user dashboard once implemented
       redirect_to action: 'show', id: 1, status: :not_found
     end
   end
