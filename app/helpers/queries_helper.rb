f5a9e785fdbb6862e05a0c19a31ee4bb1e5a3540
haleymnatz <haleymnatz@gmail.com>
2017-07-19 13:58:26 -0400

    Basic recipe info visible on queries#index

--- app/helpers/queries_helper.rb
+++ app/helpers/queries_helper.rb
@@ -1,2 +1,15 @@
 module QueriesHelper
+  require 'tasks/recipe_dto'
+
+  def hits
+    self['hits']
+  end
+
+  def make_dto
+    RecipeDto.new(self['recipe'])
+  end
 end
+
+# @recipes == HTTParty::Response
+# @recipes['hits'] == Array
+# @recipes['hits'].each --> recipe['recipe'] == Hash
