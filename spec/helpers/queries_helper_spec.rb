317ba04289687bebdcbb49cd0425f274f6befb84
haleymnatz <haleymnatz@gmail.com>
2017-07-19 14:33:33 -0400

    Add calories to query results and write tests for queries helper methods

--- spec/helpers/queries_helper_spec.rb
+++ spec/helpers/queries_helper_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
+require 'tasks/query_result'
+require 'tasks/recipe_dto'
+file = File.read('spec/data.json')
+data_hash = JSON.parse(file)
 
 # Specs in this file have access to a helper object that includes
 # the QueriesHelper. For example:
@@ -11,4 +15,21 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe QueriesHelper, type: :helper do
+  describe 'hits' do
+    it 'returns the hits from the httparty response object' do
+      expect(data_hash.hits).to eq(data_hash['hits'])
+    end
+  end
+
+  describe 'make_dto' do
+    let(:recipe) { data_hash.hits[0] }
+
+    it 'creates a RecipeDto object from the recipe json data' do
+      expect(recipe.make_dto.class).to eq(RecipeDto.new(recipe['recipe']).class)
+    end
+
+    it 'creates a RecipeDto object that has the correct values (testing label)' do
+      expect(recipe.make_dto.label).to eq(RecipeDto.new(recipe['recipe']).label)
+    end
+  end
 end
