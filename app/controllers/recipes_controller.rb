bc9dbf81d4d0e3e058a992db7cd1f6464e280d06
lnzd17 <lindsaykayedutton@gmail.com>
2017-07-26 14:48:52 -0400

    added user conneciton to recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,7 @@ class RecipesController < ApplicationController
   before_action :authenticate_user!, only: %i[create destroy]
 
   def index
-    recipes = Recipe.all
+    recipes = current_user.recipes.all
     render json: recipes
   end
 
@@ -25,12 +25,13 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = Recipe.create(name: params[:name], edamam_id: params[:id])
+    @recipe = current_user.recipes.create(name: params[:name],
+                                          edamam_id: params[:id])
     redirect_to root_path
   end
 
   def destroy
-    @recipe = Recipe.find_by(edamam_id: params[:id])
+    @recipe = current_user.recipes.find_by(edamam_id: params[:id])
     @recipe.delete
     redirect_to root_path
   end
