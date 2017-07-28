63028ed345c8eae68d4231acb5610d9291ce88fb
haleymnatz <haleymnatz@gmail.com>
2017-07-28 13:49:55 -0400

    Remove test code

--- app/assets/javascripts/queries.js
+++ app/assets/javascripts/queries.js
@@ -8,13 +8,6 @@ function changeButton() {
   });
 }
 
-function clickThings() {
-  var url = window.location.href
-  $('#search').click(function(){
-    $('#search').load(url + ' #button-762cb48b24d77730832dd762cf68478e')
-  });
-}
-
 $( document ).ready(function() {
   changeButton();
 });
