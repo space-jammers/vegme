89c855818b41074dec5baa85671dc748a407f92f
haleymnatz <haleymnatz@gmail.com>
2017-08-02 13:53:29 -0400

    Finish graph, edit query cards, write tests

--- spec/helpers/recipes_helper_spec.rb
+++ spec/helpers/recipes_helper_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
+require 'tasks/recipe_dto'
+file = File.read('spec/spicy_eggplant.json')
+data_hash = JSON.parse(file)[0]
 
 # Specs in this file have access to a helper object that includes
 # the RecipesHelper. For example:
@@ -11,4 +14,32 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe RecipesHelper, type: :helper do
+  subject(:eggplant_dto) { RecipeDto.new(data_hash) }
+
+  describe 'percent_cals_from_fat' do
+    it 'gives the percentage of total calories from fat' do
+      fat_percent = ((data_hash['totalNutrients']['FAT']['quantity'] * 9) /
+        data_hash['calories']) * 100
+      expect(RecipesHelper.percent_cals_from_fat(eggplant_dto))
+        .to eq(fat_percent)
+    end
+  end
+
+  describe 'percent_cals_from_protein' do
+    it 'gives the percentage of total calories from protein' do
+      protein_percent = ((data_hash['totalNutrients']['PROCNT']['quantity'] * 4) /
+        data_hash['calories']) * 100
+      expect(RecipesHelper.percent_cals_from_protein(eggplant_dto))
+        .to eq(protein_percent)
+    end
+  end
+
+  describe 'percent_cals_from_carbs' do
+    it 'gives the percentage of total calories from carbs' do
+      carbs_percent = ((data_hash['totalNutrients']['CHOCDF']['quantity'] * 4) /
+        data_hash['calories']) * 100
+      expect(RecipesHelper.percent_cals_from_carbs(eggplant_dto))
+        .to eq(carbs_percent)
+    end
+  end
 end
