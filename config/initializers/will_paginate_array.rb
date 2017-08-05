925afa243ac0c565d85effddf8499d30e4fe1613
lnzd17 <lindsaykayedutton@gmail.com>
2017-08-05 11:21:12 -0400

    fixed index jumpiness

--- config/initializers/will_paginate_array.rb
+++ config/initializers/will_paginate_array.rb
@@ -1,7 +1,7 @@
 require 'will_paginate/collection'
 
 Array.class_eval do
-  def paginate(page = 1, per_page = 3)
+  def paginate(page = 1, anchor= nil, per_page = 3)
     page = 1 if page.blank? # To fix weird params[:page] = nil problem
     WillPaginate::Collection.create(page, per_page, size) do |pager|
       pager.replace self[pager.offset, pager.per_page].to_a
