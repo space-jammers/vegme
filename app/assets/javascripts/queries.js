0a0733eb010f865fb5b7da403800fec99accf17b
lnzd17 <lindsaykayedutton@gmail.com>
2017-08-08 13:43:02 -0400

    updated font

--- app/assets/javascripts/queries.js
+++ app/assets/javascripts/queries.js
@@ -33,10 +33,8 @@ function showForm(){
 function dislikeDisappear(){
   $('.dislike').click(function(){
     $(this).parents('.callout').fadeTo( "1000" , 0.5, function() {
-    // Animation complete.
-  });
-    // $(this).parents('.callout').addClass('animated fadeOut');
-    // $('animated fadeOutLeft').remove();
+      // Animation complete.
+    });
   });
 }
 
