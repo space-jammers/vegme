b0f1fbc5ce7df9554f3af1871bf3db58b8dda044
lnzd17 <lindsaykayedutton@gmail.com>
2017-08-17 09:46:14 -0400

    discuss potential refactor

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -1,20 +1,13 @@
 module RecipesHelper
-  def self.recipe_dto_from_api(recipe_data)
-    recipe_call = GetRecipe.new(recipe_data.edamam_id)
-    recipe = recipe_call.find_recipe
-    RecipeErrors.store_api_result(recipe)
-    RecipeErrors.api_limit? ? return : RecipeDto.new(recipe[0])
-  end
-
-  def self.percent_cals_from_fat(recipe)
+  def percent_cals_from_fat(recipe)
     ((recipe.fat * 9) / recipe.calories) * 100
   end
 
-  def self.percent_cals_from_protein(recipe)
+  def percent_cals_from_protein(recipe)
     ((recipe.protein * 4) / recipe.calories) * 100
   end
 
-  def self.percent_cals_from_carbs(recipe)
+  def percent_cals_from_carbs(recipe)
     ((recipe.carbs * 4) / recipe.calories) * 100
   end
 end
