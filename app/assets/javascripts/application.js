6d355fc01e2f482e8716718c2594f4c4f4827df8
lnzd17 <lindsaykayedutton@gmail.com>
2017-08-18 15:15:30 -0400

    added links to footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,7 +19,7 @@ $(function(){
   $(document).foundation();
   $(window).scroll(function() {
     var winTop = $(window).scrollTop();
-    if (winTop >= 200) {
+    if (winTop >= 30) {
       $("body").addClass("sticky-shrinknav-wrapper");
     } else{
       $("body").removeClass("sticky-shrinknav-wrapper");
