a9c4c0c1790866fa7ebc078f39b0063e18d37743
lnzd17 <lindsaykayedutton@gmail.com>
2017-08-20 18:09:07 -0400

    improved transition

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,11 +21,11 @@ $(function(){
     var winTop = $(window).scrollTop();
     if (winTop >= 30) {
       $("body").addClass("sticky-shrinknav-wrapper");
-      $('.large-logo').hide();
+      $('.large-logo').hide(10);
       $('.small-logo-container').fadeIn();
     } else{
       $("body").removeClass("sticky-shrinknav-wrapper");
-      $('.large-logo').show();
+      $('.large-logo').fadeIn('slow');
       $('.small-logo-container').fadeOut();
     }
   });
