c0fc8c264bc1ec4cd337f8e9df75a6705e54d442
haleymnatz <haleymnatz@gmail.com>
2017-08-21 15:21:47 -0400

    Restyle flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def foundation_class_for flash_type
+    case flash_type
+    when :success
+      "success"
+    when :error
+      "alert"
+    when :alert
+      "warning"
+    when :notice
+      "primary"
+    else
+      "secondary"
+    end
+  end
 end
