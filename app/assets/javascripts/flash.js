55cee6c1c082d624cdfabbceaab781a24c82de5b
haleymnatz <haleymnatz@gmail.com>
2017-08-22 14:03:12 -0400

    Fix flash animation error and add fade out on close-button click

--- app/assets/javascripts/flash.js
+++ app/assets/javascripts/flash.js
@@ -1,15 +1,21 @@
 function showFlash() {
   var flashData = $( '.flash-message').attr('data-flash-type');
   if (flashData != '') {
-    // Show flash message
     $( '.flash-message' ).css('display', 'block').addClass('animated fadeInRight');
     setTimeout(function(){
-    // Hide flash message
       $( '.flash-message' ).addClass( 'animated fadeOutRight' );
     }, 3500);
 
   }
 }
+
+function closeFlash() {
+  $( '.close-button').click(function(){
+    $( '.flash-message' ).addClass( 'animated fadeOutRight' );
+  });
+}
+
 $(function(){
+  closeFlash();
   showFlash();
 });
