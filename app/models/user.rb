e3aec54cfb3f62184d8cf9117abcbc5c8c966a8f
lnzd17 <lindsaykayedutton@gmail.com>
2017-08-23 10:58:34 -0400

    added admin? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,8 @@ class User < ApplicationRecord
   def disliked_recipes
     recipes.where(dislike: true).select('edamam_id').map(&:edamam_id).to_a
   end
+
+  def admin?
+    current_user.email == ENV['hm_email'] || ENV['ld_email']
+  end
 end
