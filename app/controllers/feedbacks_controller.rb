64099d354d4384cd06b8a3d63667718869fc77b3
haleymnatz <haleymnatz@gmail.com>
2017-08-23 15:07:52 -0400

    Add content to feedback form

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -1,7 +1,23 @@
 class FeedbacksController < ApplicationController
   before_action :authenticate_user!
 
-  def new; end
+  def new
+    @feedback = Feedback.new
+  end
 
-  def create; end
+  def create
+    @feedback = current_user.feedback.create(feedback_params)
+    redirect_to queries_path
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(:label,
+                                     :message,
+                                     :link,
+                                     :user_id,
+                                     :admin_id,
+                                     :complete)
+  end
 end
