f38f0996de8e7abdac096d148f85433bc15c2c9c
haleymnatz <haleymnatz@gmail.com>
2017-08-24 18:24:52 -0400

    Helper method to find admin ids based on ENV emails

--- app/helpers/feedbacks_helper.rb
+++ app/helpers/feedbacks_helper.rb
@@ -1,2 +1,8 @@
 module FeedbacksHelper
+  def admin_ids
+    l_id = User.find_by(email: ENV['ld_email']).id
+    h_id = User.find_by(email: ENV['hm_email']).id
+    ids = [l_id, h_id]
+    ids
+  end
 end
