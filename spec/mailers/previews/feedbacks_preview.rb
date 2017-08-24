e75179b79e3dd5bb650626c8729fd2ef8f242a8b
lnzd17 <lindsaykayedutton@gmail.com>
2017-08-24 07:28:28 -0400

    revised message

--- spec/mailers/previews/feedbacks_preview.rb
+++ spec/mailers/previews/feedbacks_preview.rb
@@ -1,3 +1,15 @@
 # Preview all emails at http://localhost:3000/rails/mailers/feedbacks
 class FeedbacksPreview < ActionMailer::Preview
+  # def user_create
+  #   User.create(email: 'bubba@here.com', password: 'sdkjh59sda')
+  # end
+  # def feedback_create
+  #   Feedback.create(user_id: user_create.id,
+  #                   label: 'idea',
+  #                   message: 'test',
+  #                   link: 'www.cats.com')
+  # end
+  # def admin_notification
+  #   FeedbacksMailer.admin_notification(feedback_create)
+  # end
 end
