e6681b14cf7ecb8922e70dfabeca7a8577893d2f
haleymnatz <haleymnatz@gmail.com>
2017-08-24 21:48:03 -0400

    Admin IDs can be selected by initials

--- app/assets/javascripts/feedbacks.js
+++ app/assets/javascripts/feedbacks.js
@@ -2,6 +2,7 @@ function adminChange() {
   $('.feedback-admin-form').click(function(){
     this.submit(function(e){
       e.preventDefault();
+      console.log("SUCCESS");
     });
   });
 }
--- app/helpers/feedbacks_helper.rb
+++ app/helpers/feedbacks_helper.rb
@@ -1,8 +1,8 @@
 module FeedbacksHelper
-  def admin_ids
+  def self.admin_ids
     l_id = User.find_by(email: ENV['ld_email']).id
     h_id = User.find_by(email: ENV['hm_email']).id
-    ids = [l_id, h_id]
+    ids = [[l_id, 'LD'], [h_id, 'HM']]
     ids
   end
 end
