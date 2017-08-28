e92b1f90cc100dafa0fe7d533260b8cd2554fc73
lnzd17 <lindsaykayedutton@gmail.com>
2017-08-28 11:46:44 -0400

    refactor tests

--- spec/controllers/dashboards_controller_spec.rb
+++ spec/controllers/dashboards_controller_spec.rb
@@ -4,30 +4,32 @@
 # was the correct object stored in the response template?
 # was the appropriate message displayed to the user in the view?
 
-require 'rails_helper'
 RSpec.describe DashboardsController, type: :controller do
   let(:user) { User.create!(email: 'monkey@here.com', password: 'sdkjh59sda') }
 
   describe 'show' do
-    it 'returns dashboard#show for authenticated user' do
-      sign_in user
-      get :show
-      expect(response).to have_http_status(:success)
+    context 'when a user is authenticated' do
+      it 'returns dashboard#show' do
+        sign_in user
+        get :show
+        expect(response).to have_http_status(:success)
+      end
     end
 
-    it 'does not allow an unathenticated user access' do
-      get :show
-      expect(response).to have_http_status(302)
-    end
+    context 'when a user is unauthenticated' do
+      before(:each) { get :show }
 
-    it 'redirects an unathenticated user to the sign in page' do
-      get :show
-      expect(response).to redirect_to new_user_session_path
-    end
+      it 'does not allow an unathenticated user access' do
+        expect(response).to have_http_status(302)
+      end
 
-    it 'flashes a message to the unathenticated user' do
-      get :show
-      expect(subject.request.flash[:alert]).to_not be_nil
+      it 'redirects an unathenticated user to the sign in page' do
+        expect(response).to redirect_to new_user_session_path
+      end
+
+      it 'flashes a message to the unathenticated user' do
+        expect(subject.request.flash[:alert]).to_not be_nil
+      end
     end
   end
 end
