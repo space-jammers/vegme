473c2493a395569ed186b447d3dd1c8ef504a517
lnzd17 <lindsaykayedutton@gmail.com>
2017-08-28 12:55:19 -0400

    added template for tests

--- spec/controllers/queries_controller_spec.rb
+++ spec/controllers/queries_controller_spec.rb
@@ -1,10 +1,30 @@
 require 'rails_helper'
 RSpec.describe QueriesController, type: :controller do
   describe 'index' do
-
   end
+
   describe 'search' do
-
   end
-  
+
+  describe 'api_call' do
+  end
+
+  describe 'empty_query' do
+  end
+
+  describe 'store' do
+  end
+
+  describe 'flash_errors' do
+  end
+
+  describe 'filter' do
+  end
+
+  describe 'un_filtered' do
+  end
+
+  describe 'verify_call' do
+  end
+
 end
