08a55fb369b45e3df69caeb8a91e1c960b1eb0f4
lnzd17 <lindsaykayedutton@gmail.com>
2017-08-29 12:49:47 -0400

    made application methods private

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :store_location, unless: :devise_controller?
 
+  private
+  
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || dashboard_path
   end
@@ -14,7 +16,7 @@ class ApplicationController < ActionController::Base
     stored_location_for(resource) || root_path
   end
 
-  private
+
 
   def feedback_params
     params.require(:feedback).permit(:label,
