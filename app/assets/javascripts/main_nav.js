a72f33d15e9c3886a78ebec03d8d3f2e39b932d7
haleymnatz <haleymnatz@gmail.com>
2017-09-01 11:01:47 -0400

    Menu overlays page content on open

--- app/assets/javascripts/main_nav.js
+++ app/assets/javascripts/main_nav.js
@@ -1,20 +1,20 @@
-function moveBody() {
-  var checked = $('#nav-trigger').prop('checked');
-  if ( checked == true ) {
-    $('#bodyId').addClass('move-body');
-  } else {
-    $('#bodyId').removeClass('move-body');
-  }
-}
-
-function checkSize () {
-  $('#nav-trigger').click(function() {
-    if ($(window).width() <= 730) {
-      moveBody();
-    }
-  });
-}
-
-$(function(){
-  checkSize();
-});
+// function moveBody() {
+//   var checked = $('#nav-trigger').prop('checked');
+//   if ( checked == true ) {
+//     $('#bodyId').addClass('move-body');
+//   } else {
+//     $('#bodyId').removeClass('move-body');
+//   }
+// }
+//
+// function checkSize () {
+//   $('#nav-trigger').click(function() {
+//     if ($(window).width() <= 730) {
+//       moveBody();
+//     }
+//   });
+// }
+//
+// $(function(){
+//   checkSize();
+// });
